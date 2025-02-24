7a987d1724fdcc5d50ced67e85c9105eec5693a4
warlyn <warlyn200@gmail.com>
2025-02-23 23:36:07 -0400

    Add `username` to users table and implement basic auth middleware
    This commit introduces a migration to add a nullable `username` column to the users table. Additionally, a new middleware `AuthenticateWithBasicAuth` is implemented to enable HTTP Basic Authentication using the `username` field.

---
+++ src/Middleware/AuthenticateWithBasicAuth.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AuthMiddlewares\Middlewares;
+
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthenticateWithBasicAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return Auth::onceBasic('username') ?: $next($request);
+    }
+}
